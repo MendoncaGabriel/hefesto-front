a9cb63ebb4e95beaf2956f67a0e74882839e7438
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-06-17 11:09:06 -0400

    finisha chart

--- src/app/metric-chart/metric-chart.component.ts
+++ src/app/metric-chart/metric-chart.component.ts
@@ -1,30 +1,58 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-metric-chart',
+  standalone: true,
   templateUrl: './metric-chart.component.html',
-  imports: [CommonModule, NgFor]
+  imports: [CommonModule, NgFor],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MetricChartComponent {
   largura = 500;
   altura = 300;
+  dados = signal<number[]>(Array.from({ length: 30 }, () => 100));
+  cdr = inject(ChangeDetectorRef);
+  speedAnimation: number = 300
 
-  dados = [10, 20, 15, 25, 30, 28, 40, 45, 35, 50];
+  constructor() {
+    this.iniciarAtualizacao();
+  }
+
+  iniciarAtualizacao() {
+    const atualizar = () => {
+      const novosDados = [...this.dados().slice(1), this.numeroAleatorio()];
+      this.dados.set(novosDados);
+      this.cdr.markForCheck();
+      setTimeout(() => requestAnimationFrame(atualizar), this.speedAnimation);
+    };
+    requestAnimationFrame(atualizar);
+  }
+
+  numeroAleatorio() {
+    const dadosAtuais = this.dados();
+    const ultimoValor = dadosAtuais[dadosAtuais.length - 1] ?? 100;
+    const variacao = Math.floor(Math.random() * 21) - 10; // entre -10 e +10
+    const novoValor = Math.max(0, Math.min(100, ultimoValor + variacao)); // mantém entre 0 e 100
+    return novoValor;
+  }
 
   get linhasY() {
     return Array.from({ length: 11 }, (_, i) => i * (this.altura / 10));
   }
 
   get linhasX() {
-    return Array.from({ length: 11 }, (_, i) => i * (this.largura / 10));
+    const dados = this.dados();
+    return Array.from({ length: dados.length }, (_, i) => i * (this.largura / dados.length));
   }
 
   get linhaCurva() {
-    const escalaX = this.largura / (this.dados.length - 1);
+    const dados = this.dados();
+    const escalaX = this.largura / (dados.length - 1);
     const escalaY = this.altura / 100;
 
-    const pontos = this.dados.map((valor, i) => {
+    const pontos = dados.map((valor, i) => {
       const x = i * escalaX;
       const y = this.altura - valor * escalaY;
       return { x, y };
@@ -42,5 +70,9 @@ export class MetricChartComponent {
     }
 
     return d;
+  }
+
+  trackByIndex(index: number): number {
+    return index;
   }
 }
